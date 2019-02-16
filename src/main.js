8592f9ba596cb25e17848b9d0a63abbb2841ad07
zengxingfu <zengxingfu@gmail.com>
2019-02-17 05:37:33 +0800

    change favicon; update baseURL

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import 'material-design-lite/material.min.js'
 
 Vue.config.productionTip = false
 Vue.prototype.$request = axios.create({
-  baseURL: 'http://weibo-sentiment.zengxingfu.com/api'
+  baseURL: 'https://anime-bang.zengxingfu.cn/api/'
 })
 dayjs.extend(relativeTime)
 dayjs.locale('zh-cn')
